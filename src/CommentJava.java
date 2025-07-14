2d82136323369baa5a44ec8e505c3237b9b7efdd
kimgy <kimgy98@creamhouse.net>
2025-07-14 10:00:42 +0900

    init 및 시스템 출력 코드 작업

---
+++ src/CommentJava.java
@@ -0,0 +1,8 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1");//hello java1을 출력
+        //System.out.println("hello java2");
+        /*System.out.println("hello java3");
+        System.out.println("hello java3");*/
+    }
+}
