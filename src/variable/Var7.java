63ea8c4f47217028de30ae0f48bfbb007a914c11
kimgayeons <4402aaa@gmail.com>
2025-08-18 08:56:39 +0900

    변수 선언 부분 작업

---
+++ src/variable/Var7.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true;
+        char d = 'A';
+        String e = "Hello";//문자열을 다루기 위한 특별한 타입
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("d: " + d);
+        System.out.println("e: " + e);
+    }
+}
