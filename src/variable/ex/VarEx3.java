4b86c3c7b13e7a2d56848c5ba92d7f6f72cf04d9
kimgayeons <4402aaa@gmail.com>
2025-08-18 09:21:21 +0900

    변수 선언 문제

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long data = 10000000000L;
+        boolean data2 = true;
+
+        System.out.println("data:"+data);
+        System.out.println("data2:"+data2);
+    }
+}
